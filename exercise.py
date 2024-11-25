085fada00c587c8cf40856d0ec368df8bca80c1f
DrueWolf <dwolf0305@gmail.com>
2024-11-24 19:58:06 -0500

    init commit

---
+++ exercise.py
@@ -0,0 +1,21 @@
+# Exercise 0: Example
+#
+# This is a practice exercise to help you understand how to write code "inside" a provided Python function.
+#
+# We'll create a function that checks a condition and prints a specific greeting message based on that condition.
+#
+# Requirements:
+# - The function is named `print_greeting`.
+# - Inside the function, declare a variable `python_is_fun` and set it to `True`.
+# - Use a conditional statement to check if `python_is_fun` is `True`.
+# - If `python_is_fun` is `True`, print the message "Python is fun!"
+
+def print_greeting():
+    # Your code goes here. Remember to indent!
+    python_is_fun = True
+    if python_is_fun:
+        print("Python is fun!")
+
+# Call the function
+print_greeting()
+
